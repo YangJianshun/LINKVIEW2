e6199cfc39e2a5f7ac639daf1a3bfd072ef3abfc
YangJianshun <shunlintianxia@sina.com>
2021-09-04 22:57:48 +0800

    fix: gitignore 修改，忽略 lib 目录

--- packages/linkview-align-parser/lib/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HELLO = void 0;
-exports.HELLO = 'hello LINKVIEW2';
--- packages/linkview-cli/lib/index.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const linkview_align_parser_1 = require("@linkview/linkview-align-parser");
-console.log(linkview_align_parser_1.HELLO);
